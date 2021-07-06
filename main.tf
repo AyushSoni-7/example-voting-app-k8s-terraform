d581edaf9949ababa1332b4cbdaa83e8da93af06
Ayush Soni <ayushsoni47.as@gmail.com>
2021-07-06 09:11:04 +0000

    updated main.tf code to include worker

--- main.tf
+++ main.tf
@@ -120,3 +120,36 @@ resource "helm_release" "resultapp" {
     local.result_app_values
   ]
 }
+
+resource "kubernetes_deployment" "worker" {
+  metadata {
+    name = "worker"
+    labels = {
+      app = "worker"
+    }
+    namespace = local.app_namespace
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "worker"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "worker"
+        }
+      }
+      spec {
+        container {
+          image = "dockersamples/examplevotingapp_worker"
+          name  = "worker-container"
+        }
+      }
+    }
+  }
+}
+
+
